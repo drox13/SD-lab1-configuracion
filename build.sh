16f0fa08de533a25052841e09e653ad3c0ac6ceb
Dario Leonidas Baron <47702340+drox13@users.noreply.github.com>
2021-08-31 00:59:52 -0500

    Update build.sh
    se crea una imagen diferente para cada contenenedor(instancias)

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-docker build -t instanciasimg .
+docker build -t instancia1img .
+docker build -t instancia2img .
 docker network create --subnet=119.18.0.0/16 mynet
 
-docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instanciasimg
-docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instanciasimg
+docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancias1img
+docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancias2img
 
 docker exec instancia1 bash -c "/etc/init.d/ssh start; /etc/init.d/ssh status; pm2 status"
 docker exec instancia2 bash -c "/etc/init.d/ssh start; /etc/init.d/ssh status; pm2 status"
