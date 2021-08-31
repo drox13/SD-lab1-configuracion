bef46e319a39742bd0ae06fc1241f2ebe5b3b059
Dario Leonidas Baron <47702340+drox13@users.noreply.github.com>
2021-08-31 01:34:38 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ docker build -t instancia1img .
 docker build -t instancia2img .
 docker network create --subnet=119.18.0.0/16 mynet
 
-docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancias1img
-docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancias2img
+docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia1img
+docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancia2img
 
 docker exec instancia1 bash -c "/etc/init.d/ssh start; /etc/init.d/ssh status; pm2 status"
 docker exec instancia2 bash -c "/etc/init.d/ssh start; /etc/init.d/ssh status; pm2 status"
